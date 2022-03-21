83e68a48a407549b445f47a65c22949d24b9a8a4
todelusion <todelusion.gmail.com>
2022-03-22 00:10:36 +0800

    2022.03.22_landingPage

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,18 +1,15 @@
 <template>
-<div class="layout">
-  <div class="max-w-[1440px] m-auto"> 
-    <header class="flex justify-between items-center px-10 py-6 border-b-2">
-      <g-link class="flex items-center">
-        <g-image alt="mwwte_LOGO" src="~/assets/LOGO.png" width="100" />
-      </g-link>
-      <div class="">
-        <g-link to="/" class="mx-5 hover:bg-[#FEC6B5] font-serif font-medium duration-100">BLOG</g-link>
-        <g-link to="/About/" class="mx-5 hover:bg-[#FEC6B5] font-serif font-medium duration-100">ABOUT</g-link>
-        <g-link class="mx-5 hover:bg-[#FEC6B5] font-serif font-medium duration-100">PODCAST</g-link>
-      </div>
-    </header>
-    <slot />
-  </div>
+<div class="landingPage h-screen">
+  <div class="max-w-[1440px] m-auto flex items-end"> 
+    <g-link class="flex">
+      <g-image alt="mwwte_LOGO" src="~/assets/LOGO.png" width="100" />
+    </g-link>
+    <div class="">
+      <g-link to="/" class="mx-5 hover:bg-[#FEC6B5] font-serif font-medium duration-100">BLOG</g-link>
+      <g-link to="/About/" class="mx-5 hover:bg-[#FEC6B5] font-serif font-medium duration-100">ABOUT</g-link>
+      <g-link class="mx-5 hover:bg-[#FEC6B5] font-serif font-medium duration-100">PODCAST</g-link>
+    </div>
+</div>
 </div>
 
 </template>
@@ -22,11 +19,22 @@ export default {
   metaInfo: {
     link: [
       {
-        rel: 'Shortcut Icon"',
-        type: 'image/ico',
-        href: '/static/favicon.ico',
+        rel: 'favicon',
+        href: 'favicon.ico'
       }
     ]
   }
 }
 </script>
+
+<style>
+body {
+  background-color: #F5F5F5;
+}
+.landingPage {
+  background-image: url("~@/assets/landingPage.jpg");
+  background-size: cover;
+  background-position: center;
+}
+
+</style>
