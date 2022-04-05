4946560d77f567ec6af891ce155c5ca1c13d0e98
todelusion <todelusion@gmail.com>
2022-04-05 20:28:03 +0800

    2022.04.05_landingPage done

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,47 +1,25 @@
 <template>
   <div class="layout">
-    <header
-      class="absolute top-0 left-0 w-full px-20 py-7 z-[1000]"
-    >
+    <header class="absolute top-0 left-0 z-[1000] w-full px-20 py-7">
       <nav class="flex w-full" :class="{ fixed: active }">
         <!-- 這邊使用scroll監聽事件改為css fixed讓滑鼠往下滾動後能維持在固定位置 -->
         <g-link to="/">
           <g-image alt="mwwte_LOGO" src="~/assets/LOGO.png" width="125" />
         </g-link>
-        <ul class="flex justify-center items-center text-black">
-          <li class="ml-20">
-            <g-link
-              to="/blog/"
-              >BLOG</g-link
-            >
+        <ul class="flex items-center justify-center text-white font-serif text-xl font-black tracking-wider">
+          <li class="ml-16 hover:bg-[#FDC6B5] hover:text-black p-3 rounded-2xl">
+            <g-link to="/blog/">BLOG</g-link>
           </li>
-          <li class="ml-20">
-            <g-link
-              to="/About/"
-              >ABOUT</g-link
-            >
+          <li class="ml-16 hover:bg-[#FDC6B5] hover:text-black p-3 rounded-2xl">
+            <g-link to="/About/">ABOUT</g-link>
           </li>
-          <li class="ml-20">
-            <g-link
-              >PODCAST</g-link
-            >
+          <li class="ml-16 hover:bg-[#FDC6B5] hover:text-black p-3 rounded-2xl">
+            <g-link>PODCAST</g-link>
           </li>
         </ul>
       </nav>
     </header>
-    <section class="relative w-full h-screen">
-      <g-image id="landingPage" src="~/assets/landingPage.jpg" class="absolute object-cover w-full h-full z-10"></g-image>
-    </section>
-
-
-    <div class="relative z-20">
-      <h1>TEST</h1>
-      <p class="text-2xl">
-        Lorem ipsum dolor sit amet consectetur, <br />adipisicing elit. Illo
-        similique velit modi magnam <br />sint reiciendis optio excepturi
-        cupiditate laborum necessitatibus.
-      </p>
-    </div>
+    <slot />
   </div>
 </template>
 
@@ -54,18 +32,18 @@ export default {
   },
   created() {
     window.addEventListener("scroll", this.handleScroll);
-    window.addEventListener("scroll", this.parallaxScroll)
+    window.addEventListener("scroll", this.parallaxScroll);
   },
   methods: {
     handleScroll() {
-      console.log('fixed');
+      console.log("fixed");
       this.active = window.scrollY > 28 ? true : false;
     },
     parallaxScroll() {
-      console.log('parallax');
-      let landingPage = document.getElementById('landingPage')
+      console.log("parallax");
+      let landingPage = document.getElementById("landingPage");
       let valueY = window.scrollY;
-      landingPage.style.top = valueY * 0.5 + 'px';
+      landingPage.style.top = valueY * 0.5 + "px";
     },
   },
 };
@@ -76,17 +54,21 @@ export default {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  scroll-behavior: smooth;
 }
 body {
   min-height: 100vh;
   background-color: #f5f5f5;
+}
+#landingPage {
+  filter: brightness(0.75)
 }
 section::before {
   content: "";
   position: absolute;
   bottom: 0;
   background: #fff;
-  height: 200px;
+  height: 100px;
   width: 100%;
   z-index: 10000;
 }
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,22 @@
 <template>
-  <Layout> </Layout>
+  <Layout>
+    <section class="relative h-screen w-full">
+      <g-image
+        id="landingPage"
+        src="~/assets/landingPage.jpg"
+        class="absolute z-10 h-full w-full object-cover"
+      ></g-image>
+    </section>
+
+    <div class="relative z-20 bg-white">
+      <h1>TEST</h1>
+      <p class="text-2xl">
+        Lorem ipsum dolor sit amet consectetur, <br />adipisicing elit. Illo
+        similique velit modi magnam <br />sint reiciendis optio excepturi
+        cupiditate laborum necessitatibus.
+      </p>
+    </div>
+  </Layout>
 </template>
 
 <script>
