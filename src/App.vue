14475a123cd06d589d93f909e50c46c61c88cba6
todelusion <todelusion@gmail.com>
2022-05-28 23:19:02 +0800

    router

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,95 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <header
+    class="top-0 left-0 z-[1000] flex w-full items-center justify-center px-20 py-7 sm:justify-start md:absolute"
+  >
+    <nav
+      class="fixed top-0 left-0 w-full px-20 flex justify-center md:justify-start"
+      :class="{ bg_black: active, bg_none: !active }"
+    >
+      <router-link to="/">
+        <img
+          alt="mwwte_LOGO"
+          src="./assets/LOGO.png"
+          width="100"
+          class="min-w-[100px] duration-150 hover:scale-105"
+        >
+      </router-link>
+      <ul
+        class="hidden items-center justify-center font-serif text-lg font-black tracking-wider text-white sm:flex"
+      >
+        <li
+          class="bg-main ml-16 rounded-full bg-primary p-3 text-gray-900 duration-150 hover:scale-105"
+        >
+          <router-link to="/about">ABOUT</router-link>
+        </li>
+        <li
+          class="bg-main ml-16 rounded-full bg-primary p-3 text-gray-900 duration-150 hover:scale-105"
+        >
+          <router-link to="/blog">BLOG</router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <router-view />
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      active: false,
+    };
+  },
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
+    window.addEventListener("scroll", this.parallaxScroll);
+  },
+  methods: {
+    handleScroll() {
+      this.active = window.scrollY > 28 ? true : false;
+    },
+    parallaxScroll() {
+      let landingPage = document.getElementById("landingPage");
+      let valueY = window.scrollY;
+      let screenX = document.body.clientWidth;
+      if (screenX>=768){
+        landingPage.style.top = valueY * 0.5 + "px";
+      }
+    },
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
+body {
+  min-height: 100vh;
+  background-color: #f5f5f5;
+  scroll-behavior: smooth;
+}
+#landingPage {
+  filter: brightness(0.75);
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+@media (min-width: 768px) {
+  .bg_black {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    padding-left: 5rem;
+    padding-right: 5rem;
+    transition: background-color 0.2s ease-in-out;
+    background: rgba(0, 0, 0, 0.7);
+  }
+  .bg_none {
+    background: rgba(0, 0, 0, 0);
+    transition: background-color 0.2s ease-in-out;
+  }
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,31 @@
 module.exports = {
-    mode: 'jit',
-    content: ["./src/**/*.{html,js}"],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {
-        colors: {
-          'primary': '#FEC6B5',
-          'second': '#E07658',
-          'third': '#F6F6F6'
-        }
-      },
-      fontFamily: {
-        sans: ['Noto Sans TC','Roboto'],
-        serif: ['Noto Serif TC','Times'],
-        dela: ['Dela Gothic One']
+  mode: 'jit',
+  purge: [
+    './src/**/*.vue',
+    './src/**/*.js',
+    './src/**/*.jsx',
+    './src/**/*.html',
+    './src/**/*.pug',
+    './src/**/*.md'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#FEC6B5',
+        'second': '#E07658',
+        'third': '#F6F6F6',
+        'dark': '#2c3e50'
       }
     },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  
+    fontFamily: {
+      sans: ['Noto Sans TC','Roboto'],
+      serif: ['Noto Serif TC','Times'],
+      dela: ['Dela Gothic One']
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
